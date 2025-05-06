4919d78159fdfd91c12be93ab6d0ab9b7d4c4a49
Arseniy <arsen190104@gmail.com>
2025-05-06 16:59:20 +0700

    Добавил контекст бд

---
+++ CompanyModule.Domain/Entities/CompanyPerson.cs
@@ -0,0 +1,27 @@
+﻿using Shared.Domain.Entites;
+
+namespace CompanyModule.Domain.Entities
+{
+    public abstract class CompanyPerson : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telegram { get; set; }
+        public string Phone { get; set; }
+        public Guid UserId { get; set; }
+        public Company Company { get; set; }
+
+        protected CompanyPerson() {}
+    }
+
+    public class Curator : CompanyPerson
+    {
+        public Curator() {}
+    }
+
+    public class CompanyRepresenter : CompanyPerson
+    {
+        public CompanyRepresenter() {}
+    }
+}
