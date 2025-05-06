e83c50606400816b8d4ddf30bc594b4cd5f80e46
Arseniy <arsen190104@gmail.com>
2025-05-06 16:59:20 +0700

    Добавил контекст бд

---
+++ CompanyModule.Domain/Entities/Appointment.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyModule.Domain.Entities
+{
+    public class Appointment : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Company Company { get; set; }
+        public List<Attachment> Attachments { get; set; } = new List<Attachment>();
+    }
+}
