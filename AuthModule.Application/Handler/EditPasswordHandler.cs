05cc085bf24e18bb7d680ba2ddc29879d01d8e84
mask321429 <128976851+mask321429@users.noreply.github.com>
2025-05-19 20:38:08 +0700

    Auth module (#14)
    * login-getRole
    
    * d
    
    * d
    
    * f
    
    * dd
    
    * df
    
    * update
    
    login, logout, refresh, UploadExcel
    
    * update
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * CreateAspNetUserHandler
    
    * EditPassword

---
+++ AuthModule.Application/Handler/EditPasswordHandler.cs
@@ -0,0 +1,38 @@
+using System.Security.Cryptography;
+using AuthModel.Infrastructure;
+using AuthModel.Service.Interface;
+using AuthModule.Contracts.CQRS;
+using AuthModule.Domain.Entity;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared.Domain.Exceptions;
+
+namespace AuthModel.Service.Handler;
+
+public class EditPasswordHandler : IRequestHandler<EditPasswordQuery>
+{
+    private readonly IHashService hashService;
+    private readonly AuthDbContext context;
+    public EditPasswordHandler(IHashService hashService, AuthDbContext context)
+    {
+        this.hashService = hashService;
+        this.context = context;
+    }
+    
+    
+    public async Task Handle(EditPasswordQuery request, CancellationToken cancellationToken)
+    { 
+        using SHA256 sha256Hash = SHA256.Create();
+        var hash = hashService.GetHash(sha256Hash, request.OldPassword);
+        
+        var aspNetUser = await context.AspNetUsers.Where(x => x.Id.Equals(request.UserId) && x.Password == hash).FirstOrDefaultAsync();
+
+        if (aspNetUser == null)
+        {
+            throw new NotFound("aspNetUser not found");
+        }
+        var newHash = hashService.GetHash(sha256Hash, request.NewPassword);
+        aspNetUser.Password = newHash;
+        await context.SaveChangesAsync();
+    }
+}
--- AuthModule.Controlllers/AuthController.cs
+++ AuthModule.Controlllers/AuthController.cs
@@ -2,6 +2,7 @@
 using AuthModule.Contracts.Model;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Domain.Exceptions;
 
 namespace AuthModule.Controlllers;
 
@@ -21,6 +22,23 @@ public class AuthController: ControllerBase
     {
         var token = await mediator.Send(loginDTO);
         return Ok(new { Token = token });
+    }
+    [HttpPut("edit/pass")]
+    [Authorize]
+    public async Task<IActionResult> Login(EditPasswordDTO password)
+    {
+        var userId = User.FindFirst("UserId")?.Value;
+        if (userId == null) throw new Unauthorized("UserId - not found");
+        
+        var newPassword = new EditPasswordQuery()
+        {
+            OldPassword = password.OldPassword,
+            NewPassword = password.NewPassword,
+            UserId = Guid.Parse(userId),
+            Login = password.Login,
+        };
+        await mediator.Send(newPassword);
+        return Ok();
     }
     
     [HttpPost("getRoleById")]
