312665e1d99bfbbd1038a5695c5e6a3dca9d0322
zzizan <romka-pol-16@mail.ru>
2025-05-22 03:45:49 +0700

    get settings handler

---
+++ AppSettingsModule.Application/Handlers/GetSettingsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AppSettingsModule.Contracts.DTOs;
+using AppSettingsModule.Contracts.Queries;
+using AppSettingsModule.Contracts.Repositories;
+using MediatR;
+using Shared.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingsModule.Application.Handlers
+{
+    public class GetSettingsQueryHandler : IRequestHandler<GetSettingsQuery, SettingsDto>
+    {
+        private readonly ISettingsRepository _settingsRepository;
+
+        public GetSettingsQueryHandler(ISettingsRepository settingsRepository)
+        {
+            _settingsRepository = settingsRepository;
+        }
+        public async Task<SettingsDto> Handle(GetSettingsQuery request, CancellationToken cancellationToken)
+        {
+            var settings = await _settingsRepository.GetSettingsByUserIdAsync(request.userId)
+               ?? throw new NotFound("Settings not found");
+
+            return new SettingsDto(settings);
+        }
+    }
+}
---
+++ AppSettingsModule.Contracts/Queries/GetSettingsQuery.cs
@@ -0,0 +1,12 @@
+﻿using AppSettingsModule.Contracts.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettingsModule.Contracts.Queries
+{
+    public record GetSettingsQuery(Guid userId) : IRequest<SettingsDto>;
+}
