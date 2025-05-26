30113f65d9da36481484690476c21d40ceb4782e
Vasandr15 <vasandrey007@gmail.com>
2025-05-26 22:45:55 +0700

    small bug fixes

--- AuthModule.Contracts/Model/LoginResponseDTO.cs
+++ AuthModule.Contracts/Model/LoginResponseDTO.cs
@@ -1,7 +1,10 @@
+using UserModule.Domain.Entities;
+
 namespace AuthModule.Contracts.Model;
 
 public class LoginResponseDTO
 {
      public string AccessToken { get; set; }
     public string RefreshToken { get; set; }
+    public List<Role> Roles { get; set; }
 }
